62cb60ac3be101612e367b89c9bcad0988b68df8
Ricardo Delfin <me@rdelfin.com>
2020-02-09 16:07:55 +0000

    Fixed direction of movement

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -21,7 +21,7 @@ public class MovementScript : MonoBehaviour
 
 
     void KeyboardMovement() {
-        var move = new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), transform.position.z);
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
     }
 
