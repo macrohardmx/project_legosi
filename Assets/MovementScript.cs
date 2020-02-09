706a9d92542836af41ffe33e67e11ef6b811fcf0
Ricardo Delfin <me@rdelfin.com>
2020-02-09 15:49:09 +0000

    Added rotation to character

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -15,7 +15,29 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var move = new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0);
+
+        KeyboardMovement();
+        MouseRotation();
+    }
+
+
+    void KeyboardMovement() {
+        var move = new Vector3(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), transform.position.z);
         transform.position += move * speed * Time.deltaTime;
+    }
+
+    void MouseRotation() {
+        var mouse = ProjectedMousePoint();
+        var direction = mouse - transform.position;
+
+        var angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(-angle, new Vector3(0, 0, 1));
+    }
+
+    Vector3 ProjectedMousePoint() {
+        var cam = Camera.main;
+        var mouse_viewport = cam.ScreenToViewportPoint(Input.mousePosition);
+        var z = cam.nearClipPlane + (transform.position.z - cam.transform.position.z);
+        return cam.ViewportToWorldPoint(new Vector3(mouse_viewport.x, mouse_viewport.y, z));
     }
 }
